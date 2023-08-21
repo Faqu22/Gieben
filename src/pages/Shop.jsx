8c4117800a79a7d0cd931e5bd3e79b5c59ce0fc7
Faqu22 <facundoalvarez415@gmail.com>
2023-08-21 02:28:38 -0300

    Implementation of Shop Route

---
+++ src/pages/Shop.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../layout'
+import TreeScene from '../components/TreeScene'
+function Shop() {
+    return (
+        <Layout>
+            <TreeScene />
+        </Layout>
+    )
+}
+
+export default Shop
