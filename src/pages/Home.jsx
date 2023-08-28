a41e463b7417836df538ff0492b7d8a61025d008
Faqu22 <facundoalvarez415@gmail.com>
2023-08-27 22:25:15 -0300

    Modify all of the esthetic

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,43 +1,49 @@
 import React from 'react'
 import Layout from '../layout'
-import Slider from '../components/Slider'
-import Card from '../components/Card'
-import Statue from '../assets/statue.png'
-import Help from '../assets/apoyar.png'
-import Mision from '../assets/mision.png'
-import Origen from '../assets/origen.png'
-import Product0 from '../assets/animals/animales1.png'
-import Product1 from '../assets/figures/figuras1.png'
-import Product2 from '../assets/people/personas1.png'
-import Product3 from '../assets/product/Imagen14.png'
+import SliderCat from '../components/SliderCat'
+import BgOfA from '../assets/home/bginpart.png'
+import Product0 from '../assets/home/catalog1.png'
+import Product1 from '../assets/home/catalog2.png'
+import Product2 from '../assets/home/catalog3.png'
+import Product3 from '../assets/home/catalog4.png'
+import Product4 from '../assets/home/catalog5.png'
+import Product5 from '../assets/home/catalog6.png'
+import Product6 from '../assets/home/catalog7.png'
+import Product7 from '../assets/home/catalog8.png'
+
 function Home() {
-    const images = [Help, Mision, Origen]
     const cards = [
         { image: Product0, label: 'Animales', path: '/tienda' },
         { image: Product1, label: 'Figura', path: '/tienda' },
         { image: Product2, label: 'Personas', path: '/tienda' },
-        { image: Product3, label: 'Personaje', path: '/tienda' },
+        { image: Product3, label: 'aaa', path: '/tienda' },
+        { image: Product4, label: 'pedro', path: '/tienda' },
+        { image: Product5, label: 'Manzana', path: '/tienda' },
+        { image: Product6, label: 'mma', path: '/tienda' },
+        { image: Product7, label: 'Mandarina', path: '/tienda' },
     ]
     return (
         <Layout>
-            <div className="bg-beige flex flex-col">
-                <div className="flex items-center justify-center text-xs">
-                    <img src={Statue} className="w-80 h-auto blur-sm" />
-                    <h1 className="text-5xl font-extrabold font-petit text-bb9885 mb-16 bg-bba485 p-4 border-x-2 border-e84a5f ml-44 rounded-3xl shadow-lg flex items-center border-b-8 hover:border-blue-200 transition duration-300 text-neutral-50">
-                        Bienvenido a Gieben
-                    </h1>
-                </div>
-                <div className="mt-14">
-                    <Slider images={images} />
+            <div className="bg-e8e8e8 flex flex-col">
+                <div className="flex justify-center items-center">
+                    <img src={BgOfA} alt="Bacground" className="blur-sm bg-black bg-opacity-30" />
+                    <div className="absolute">
+                        <h1 className="text-7xl font-openSans  text-center text-second font-thin m-5">
+                            GIEBEN
+                        </h1>
+                        <h1 className="text-7xl font-openSans text-center text-second font-thin m-5">
+                            te da la
+                        </h1>
+                        <h1 className="text-7xl font-openSans text-center text-second font-thin m-5">
+                            bienvenida
+                        </h1>
+                        <span className="text-4xl not-italic">¿Que quieres crear?</span>
+                    </div>
                 </div>
                 <div>
-                    <div className="border-y-black border-y-2 py-5 header text-53433e border-opacity-10 bg-white">
-                        <h1>Catalogo</h1>
-                    </div>
+                    <br />
                     <div className="flex justify-center items-center">
-                        {cards.map(({ image, label, path }) => (
-                            <Card image={image} label={label} path={path} />
-                        ))}
+                        <SliderCat items={cards} />
                     </div>
                 </div>
             </div>
