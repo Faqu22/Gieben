6dbd80fd6ce696f9ae1f6e22a533ef7f6a92b8c5
Faqu22 <facundoalvarez415@gmail.com>
2023-08-27 22:28:50 -0300

    Adjust the size

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,15 +6,15 @@ function Card({ image, label, path }) {
     return (
         <Link
             to={path}
-            className="w-56 h-96 m-8 border bg-white hover:bg-gray-100 rounded-lg shadow-sm hover:shadow-md transform hover:scale-105 transition-transform"
+            className="w-56 h-96 m-8  rounded-lg transform hover:scale-105 transition-transform"
         >
             {/* Contenedor de imagen */}
-            <div className="flex justify-center items-center h-2/3 rounded-t-lg overflow-hidden">
+            <div className="flex justify-center items-center h-70 w-70 rounded-t-lg overflow-hidden">
                 <img src={image} alt="Product" className="max-w-full h-auto" />
             </div>
 
             {/* Contenedor del ícono */}
-            <div className="flex justify-center items-center h-1/3 bg-gray-200 rounded-b-lg">
+            <div className="flex justify-center items-center">
                 <Badge label={label} />
             </div>
         </Link>
