e82fe57dd85a4309b536b803f572688c6cf2fb84
Faqu22 <facundoalvarez415@gmail.com>
2023-08-27 22:28:09 -0300

    Modify the color

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -5,7 +5,7 @@ function Icon(props) {
     return (
         <Link
             to={props.path}
-            className="flex h-fit items-center space-x-2 font-semibold hover:bg-opacity-40 rounded-3xl px-4 bg-bba485 hover:bg-a8957a transition duration-300 text-53433e"
+            className="flex h-fit items-center space-x-2 font-semibold hover:bg-opacity-40 rounded-3xl px-4 bg-e8e8e8 hover:bg-white transition duration-300 text-53433e"
         >
             {props.icon && <img src={props.icon} alt="Icon" className="h-36" />}
             {props.name && <span>{props.name}</span>}
